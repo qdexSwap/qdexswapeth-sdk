b3755b7477b9eebe180fcf3772015683261f1ddd
Pc-3 <gopalgurram1@gmail.com>
2021-09-29 09:56:35 +0530

    package scope changed

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 'use strict'
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./qdexswapethsdk.cjs.production.min.js')
+  module.exports = require('./ethsdk.cjs.production.min.js')
 } else {
-  module.exports = require('./qdexswapethsdk.cjs.development.js')
+  module.exports = require('./ethsdk.cjs.development.js')
 }
